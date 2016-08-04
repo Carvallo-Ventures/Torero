e08121336bf1b431ca8ebf011c14a105a896f9ca
Diego Carvallo <dccakes@gmail.com>
2016-08-04 12:38:58 -0400

    sidebar nav styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
+before_action :authenticate_user!, only: [:dashboard]
 
 	def index
 	end
 	
+	def dashboard
+		@user=current_user
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   resources :rotations 
-  
   devise_for :users
   root 'welcome#index'
+  get '/dashboard' => 'welcome#dashboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
